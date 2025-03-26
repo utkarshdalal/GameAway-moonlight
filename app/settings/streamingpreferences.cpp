d323b0ebd69ffe3c1dca4a1e3959d5d8b110dca0
Utkarsh Dalal <utkarsh.dalal@toptal.com>
2025-03-26 16:41:22 +0530

    Changed default bitrate to 5000 (since it's in kbps)

--- app/settings/streamingpreferences.cpp
+++ app/settings/streamingpreferences.cpp
@@ -120,7 +120,7 @@ void StreamingPreferences::reload()
     height = settings.value(SER_HEIGHT, 1080).toInt();
     fps = settings.value(SER_FPS, 60).toInt();
     enableYUV444 = settings.value(SER_YUV444, false).toBool();
-    bitrateKbps = settings.value(SER_BITRATE, 5).toInt();
+    bitrateKbps = settings.value(SER_BITRATE, 5000).toInt();
     unlockBitrate = settings.value(SER_UNLOCK_BITRATE, false).toBool();
     enableVsync = settings.value(SER_VSYNC, true).toBool();
     gameOptimizations = settings.value(SER_GAMEOPTS, true).toBool();
